086ee27141a2a10d8bbd41ec3702d9d48a75b5f3
Nikita Smiichyk <nikitasmiichyk@gmail.com>
2025-03-24 14:01:06 +0000

    0.6 add dragImage.js

--- js/app.js
+++ js/app.js
@@ -7,11 +7,6 @@ let filters = {}     // Object to store active filters
 // Variables for slider div elements
 let brightnessDiv, grayscaleDiv, sepiaDiv, invertDiv
 
-// Variables for dragging
-let imgPositionX = 0, imgPositionY = 0
-let isDragging = false
-let startX, startY
-
 // Create an image object and load the picture
 let img = new Image()
 
@@ -102,53 +97,4 @@ function initializeSlidersDiv() {
 function setCanvasWidthAndHeight() {
   canvas.width = canvas.clientWidth
   canvas.height = canvas.clientHeight
-}
-
-function setImagePosition() {
-  // Center the image in the canvas
-  imgPositionX= (canvas.width - img.width) / 2
-  imgPositionY = (canvas.height - img.height) / 2
-}
-
-function limitImagePosition() {
-  let margin = 0.4
-  let marginX = canvas.width * margin
-  let marginY = canvas.height * margin
-
-  let minX = Math.min(-marginX, canvas.width - img.width + marginX)
-  let maxX = Math.max(-marginX, canvas.width - img.width + marginX)
-  let minY = Math.min(-marginY, canvas.height - img.height + marginY)
-  let maxY = Math.max(-marginY, canvas.height - img.height + marginY)
-
-  imgPositionX = Math.max(minX, Math.min(imgPositionX, maxX))
-  imgPositionY = Math.max(minY, Math.min(imgPositionY, maxY))
-}
-
-function addEventListeners() {
-  canvas.addEventListener("mousedown", startDragging)
-  canvas.addEventListener("mousemove", dragImage)
-  canvas.addEventListener("mouseup", stopDragging)
-  canvas.addEventListener("mouseleave", stopDragging)
-}
-
-function startDragging(event) {
-  isDragging = true
-  startX = event.clientX - imgPositionX
-  startY = event.clientY - imgPositionY
-  canvas.style.cursor = "grabbing"
-}
-
-function dragImage(event) {
-  // Move the image while dragging
-  if (isDragging) {
-    imgPositionX = event.clientX - startX
-    imgPositionY = event.clientY - startY
-    limitImagePosition()
-    drawImage()
-  }
-}
-
-function stopDragging() {
-  isDragging = false
-  canvas.style.cursor = "grab"
 }
